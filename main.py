db0d97ab9b461d897e63656c4caf713baf3dfcb0
shepherdm <shepherdm1@wit.edu>
2024-04-02 21:02:16 -0400

    switched to new dataset, added code to create Figures and Models directories if they don't already exist, other general code cleanup

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
-import io
 import os
-import pickle
-from random import randint
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from cairosvg import svg2png
 from joblib import dump
 from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
-from keras.preprocessing.image import img_to_array, load_img
-from PIL import Image
+from keras.preprocessing.image import load_img
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
+# directory where the images are located
 path = r"FloorPlans"
-# change the working directory to the path where the images are located
-# os.chdir(path)
-
-# this list holds all the image filename
+# directory to save figures
+fig_path = "Figures"
+# directory to download save models
+model_path = "Models"
+# this list holds all the image filenames
 images = []
 
 # creates a ScandirIterator aliased as files
 with os.scandir(path) as files:
     # loops through each file in the directory
     for file in files:
-        if file.name.endswith(".png"):
+        if file.name.endswith(".jpg"):
             # adds only the image files to the flowers list
             images.append(file.name)
 
 
-model = VGG16()
-model.save("Models/vgg16.keras")
-model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-model.save("Models/vgg16_trunc.keras")
+if not os.path.exists(f"{model_path}"):
+    os.makedirs(f"{model_path}")
 
-# def svgRead(filename):
-#     """Load an SVG file and return image in Numpy array"""
-#     # Make memory buffer
-#     mem = io.BytesIO()
-#     # Convert SVG to PNG in memory
-#     svg2png(url=filename, write_to=mem)
-#     # Convert PNG to Numpy array
-#     return np.array(Image.open(mem))
+model = VGG16()
+model.save(f"{model_path}/vgg16.keras")
+model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+model.save(f"{model_path}/vgg16_trunc.keras")
 
 
 def extract_features(file, model):
-    # img = svgRead(file)
-    img = load_img(path + "\\" + file, target_size=(224, 224))
+    img = load_img(path + "/" + file, target_size=(224, 224))
     # convert from 'PIL.Image.Image' to numpy array
     img = np.array(img)
     # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
@@ -56,43 +44,36 @@ def extract_features(file, model):
     # prepare image for model
     imgx = preprocess_input(reshaped_img)
     # get the feature vector
-    features = model.predict(imgx, use_multiprocessing=True)
+    features = model.predict(imgx)#, use_multiprocessing=True)
     return features
 
 
 data = {}
 # p = r".\FeatureVectors"
 
-# lop through each image in the dataset
+# loop through each image in the dataset
 for image in images:
     # try to extract the features and update the dictionary
-    feat = extract_features(image, model)
-    data[image] = feat
-    # if something fails, save the extracted features as a pickle file (optional)
-    # except:
-    #     with open(p, "wb") as file:
-    #         pickle.dump(data, file)
+    features = extract_features(image, model)
+    data[image] = features
 
 
 # get a list of the filenames
 filenames = np.array(list(data.keys()))
 
-# get a list of just the features
-feat = np.array(list(data.values()))
-
-# reshape so that there are 119 samples of 4096 vectors
-feat = feat.reshape(-1, 4096)
+# get and reshape a list of just the features
+features = np.array(list(data.values())).reshape(-1, 4096)
 
 # reduce the amount of dimensions in the feature vector
 pca = PCA(n_components=100, random_state=22)
-pca.fit(feat)
-x = pca.transform(feat)
-dump(pca, "Models/pca.joblib")
+pca.fit(features)
+x = pca.transform(features)
+dump(pca, f"{model_path}/pca.joblib")
 
 # cluster feature vectors
 kmeans = KMeans(n_clusters=3, random_state=22)
 kmeans.fit(x)
-dump(kmeans, "Models/kmeans.joblib")
+dump(kmeans, f"{model_path}/kmeans.joblib")
 
 # holds the cluster id and the images { id: [images] }
 groups = {}
@@ -109,6 +90,9 @@ def view_cluster(cluster):
     plt.figure(figsize=(25, 25))
     # gets the list of filenames for a cluster
     files = groups[cluster]
+
+    if not os.path.exists(f"{fig_path}"):
+        os.makedirs(f"{fig_path}")
     # only allow up to 30 images to be shown at a time
     if len(files) > 3:
         print(f"Clipping cluster size from {len(files)} to 3")
@@ -116,18 +100,18 @@ def view_cluster(cluster):
     # plot each image in the cluster
     for index, file in enumerate(files):
         plt.subplot(1, 3, index + 1)
-        img = load_img(path + "\\" + file)
+        img = load_img(path + "/" + file)
         img = np.array(img)
         plt.imshow(img)
         plt.axis("off")
-    plt.savefig(f"Figures/Cluster{cluster}_small")
+    plt.savefig(f"{fig_path}/Cluster{cluster}_small")
 
 
 for key in groups.keys():
     view_cluster(key)
 
 
-# this is just incase you want to see which value for k might be the best
+# determine value for k
 # sse = []
 # list_k = list(range(3, 50))
 
